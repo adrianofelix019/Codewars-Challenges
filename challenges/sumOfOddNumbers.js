67d501325d46b69d3375b96c114edc0eae24a820
adrianofelix019 <46236684+adrianofelix019@users.noreply.github.com>
2020-05-20 10:12:06 -0300

    Create sumOfOddNumbers.js

---
+++ challenges/sumOfOddNumbers.js
@@ -0,0 +1,22 @@
+/*
+Given the triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+
+Calculate the row sums of this triangle from the row index (starting at index 1) e.g.:
+rowSumOddNumbers(1); // 1
+rowSumOddNumbers(2); // 3 + 5 = 8
+*/
+
+function rowSumOddNumbers(n) {
+  // The row sums is equal to the cube of row index
+	return n**3
+}
+
+console.log(rowSumOddNumbers(1))
+console.log(rowSumOddNumbers(2))
