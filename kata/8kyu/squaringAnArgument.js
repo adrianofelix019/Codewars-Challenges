e4843ce50d5794abdb69faae4309f858ffd4cc06
adrianofelix019 <46236684+adrianofelix019@users.noreply.github.com>
2020-11-21 15:34:39 -0300

    Create squaringAnArgument.js

---
+++ kata/8kyu/squaringAnArgument.js
@@ -0,0 +1,11 @@
+/*
+Now you have to write a function that takes an argument and returns the square of it.
+*/
+
+function square(num) {
+  return num**2
+}
+
+console.log(square(3))
+console.log(square(5))
+console.log(square(9))
