75c07efd61cbe3dd6af976f81cd727e4af2b652f
adrianofelix019 <46236684+adrianofelix019@users.noreply.github.com>
2020-11-28 16:50:47 -0300

    Create clock.js

---
+++ kata/8kyu/clock.js
@@ -0,0 +1,18 @@
+/*
+Clock shows 'h' hours, 'm' minutes and 's' seconds after midnight.
+
+Your task is to make 'Past' function which returns time converted to milliseconds.
+
+Example:
+past(0, 1, 1) == 61000
+*/
+
+function past(h, m, s) {
+  const seconds = s * 1000
+  const minutes = m * 60 * 1000
+  const hours = h * 60 * 60 * 1000
+  return seconds + minutes + hours
+}
+
+console.log(past(0,1,1)) // 61000
+console.log(past(1,1,1)) // 3661000
