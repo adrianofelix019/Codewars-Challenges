e317984289b2a0c5e3582a11df208494952e6cf6
Adriano Felix <adrianofelix019@gmail.com>
2022-03-17 07:52:53 -0300

    Solved Convert boolean values to  strings 'Yes' or 'No' using Elixir.

---
+++ kata/elixir/8kyu/Convert BooleanValuesToStringsYesOrNo.ex
@@ -0,0 +1,8 @@
+# Complete the method that takes a boolean value and return a "Yes"
+# string for true, or a "No" string for false.
+
+defmodule YesOrNo do
+  def boolToWord(true), do: "Yes"
+
+  def boolToWord(_), do: "No"
+end
