154d8539ee44713bf5895d6d7df033db358ec291
Adriano Felix <adrianofelix019@gmail.com>
2022-06-27 16:35:52 -0300

    Solved Strong Number using Elixir.

---
+++ kata/elixir/7kyu/strong_number.ex
@@ -0,0 +1,52 @@
+# Definition
+# Strong number is the number that the sum of the factorial of its digits is equal to number itself.
+#
+# For example, 145 is strong, since 1! + 4! + 5! = 1 + 24 + 120 = 145.
+#
+# Task
+# Given a number, Find if it is Strong or not and return either "STRONG!!!!" or "Not Strong !!".
+#
+# Notes
+# - Number passed is always Positive.
+# - Return the result as String
+#
+# Input >> Output Examples
+# strong_num(1) ==> return "STRONG!!!!"
+# Since, the sum of its digits' factorial (1) is equal to number itself, then its a Strong.
+#
+# strong_num(123) ==> return "Not Strong !!"
+# Since the sum of its digits' factorial of 1! + 2! + 3! = 9 is not equal to number itself, then it's Not Strong .
+#
+# strong_num(2)  ==>  return "STRONG!!!!"
+# Since the sum of its digits' factorial of 2! = 2 is equal to number itself, then its a Strong.
+#
+# strong_num(150) ==> return "Not Strong !!"
+# Since the sum of its digits' factorial of 1! + 5! + 0! = 122 is not equal to number itself, Then it's Not Strong .
+
+defmodule Kata do
+
+  defp to_integer(value) do
+    {number, _} = Integer.parse(value)
+    number
+  end
+
+  defp factorial(0), do: 1
+  defp factorial(num) do
+    num * factorial(num - 1)
+  end
+
+  def strong(num) do
+    fac_digits_sum =
+      Integer.to_string(num)
+      |> String.graphemes()
+      |> Enum.map(&to_integer/1)
+      |> Enum.map(&factorial/1)
+      |> Enum.sum()
+
+    if fac_digits_sum == num do
+      "STRONG!!!!"
+    else
+      "Not Strong !!"
+    end
+  end
+end
