cd7951bfe128499c56663d8ec1dcf348a809af9f
Adriano Felix <adrianofelix019@gmail.com>
2022-09-05 13:43:41 -0300

    Solved Testing 1-2-3 using Java.

---
+++ kata/java/7kyu/Testing123.java
@@ -0,0 +1,28 @@
+/*
+ * Your team is writing a fancy new text editor and you've been tasked with
+ * implementing the line numbering.
+ *
+ * Write a function which takes a list of strings and returns each line
+ * prepended by the correct number.
+ *
+ * The numbering starts at 1. The format is n: string. Notice the colon and
+ * space in between.
+ *
+ * Examples: (Input --> Output)
+ *
+ * [] --> []
+ * ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+ */
+
+import java.util.*;
+
+public class LineNumbering {
+    public static List<String> number(List<String> lines) {
+        List<String> result = new ArrayList<String>();
+        for(int i = 0;i < lines.size();i++) {
+          result.add((i+1)+": " + lines.get(i));
+      }
+    
+      return result;
+    }
+}
