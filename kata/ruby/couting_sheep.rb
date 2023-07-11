e9f247635e597e050c72dd00de204b7fdc44f015
Adriano Felix <adrianofelix019@gmail.com>
2023-07-11 20:34:28 -0300

    adjust in comments

--- kata/ruby/couting_sheep.rb
+++ kata/ruby/couting_sheep.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Consider an array/list of sheep where some sheep may be missing from their
 # place. We need a function that counts the number of sheep present in the 
 # array (true means present).
